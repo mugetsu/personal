c541b97e9c9aa5f2bf45bcde2c5e96f458957c24
Randell Quitain <cprjk.buzz@gmail.com>
2020-02-27 00:50:40 +0800

    Integrate firebase as database

--- store/actions.js
+++ store/actions.js
@@ -2,9 +2,8 @@ export default {
   async nuxtServerInit({ commit }, ctx) {
     const pageRef = this.$fireDb.ref('page')
     try {
-      await pageRef.once('value').then(snapshot => {
-        commit('SET_PAGE', snapshot.val())
-      })
+      const snapshot = await pageRef.once('value')
+      commit('SET_PAGE', snapshot.val())
     } catch (e) {
       return Promise.reject(e)
     }
